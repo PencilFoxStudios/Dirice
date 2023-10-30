e584abaf14de763ba896fd6801a1722a08c67d05
Liam <111019557+shackhorn@users.noreply.github.com>
2023-10-30 15:50:20 -0400

    Make DigitalOcean happy. (#3)

--- src/web/WebServer.ts
+++ src/web/WebServer.ts
@@ -7,15 +7,43 @@ export class WebServer {
     private Discord: Client;
     private EraserTail:EraserTailClient;
     private Dirice:DiriceDBClient;
-    private readonly server = fastify();
+
+    private readonly server = fastify(
+        {  logger: {
+            serializers: {
+              req: function (req) {
+                return { url: req.url }
+              }
+            }
+          }}
+    );
+
     private readonly PORT: number = 8080;
     constructor(client: Client, Dirice:DiriceDBClient, EraserTail:EraserTailClient) {
         this.Discord = client;
         this.Dirice = Dirice;
         this.EraserTail = EraserTail;
+        this.init();
+    }
+    init(){
+        const ET = this.EraserTail;
+        this.server.get('/health', function handler(request, reply) {
+            ET.log("Debug", "/health was rung!")
+            return "I'm feeling pretty good!"
+        })
+        this.server.get('/', function handler(request, reply) {
+            ET.log("Debug", "/ was rung!")
+            return "Yeah good."
+        })
+        this.server.get('/dev', function handler(request, reply) {
+            ET.log("Debug", "/dev was rung!")
+            return "Yeah goodie."
+        })
+
         this.server.get('/health', async function handler(request, reply) {
             return "I'm feeling pretty good!"
         })
+
     }
     async start() {
         try {
